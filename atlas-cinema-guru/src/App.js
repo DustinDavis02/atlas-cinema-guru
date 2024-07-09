d4751dfd5cd3cea8cf7072445753b54ef23463dd
Dustin Davis <dustindavis328@gmail.com>
2024-07-09 18:38:11 -0500

    App.js

--- atlas-cinema-guru/src/App.js
+++ atlas-cinema-guru/src/App.js
@@ -1,8 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
+import Dashboard from './routes/dashboard/Dashboard';
+import Authentication from './routes/auth/Authentication';
 
-function App() {
-  return <div className="App"></div>;
-}
+const App = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [userUsername, setUserUsername] = useState("");
+
+  useEffect(() => {
+    const accessToken = localStorage.getItem('accessToken');
+    if (accessToken) {
+      axios.post('http://localhost:8000/api/auth/', {}, {
+        headers: { Authorization: `Bearer ${accessToken}` }
+      })
+      .then(response => {
+        setIsLoggedIn(true);
+        setUserUsername(response.data.username);
+      })
+      .catch(error => {
+        console.error("Authentication failed:", error);
+      });
+    }
+  }, []);
+
+  return (
+    <div className="App">
+      {isLoggedIn ? <Dashboard userUsername={userUsername} setIsLoggedIn={setIsLoggedIn} /> : <Authentication setIsLoggedIn={setIsLoggedIn} setUserUsername={setUserUsername} />}
+    </div>
+  );
+};
 
 export default App;
